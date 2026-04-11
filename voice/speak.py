b18e286ee40c7ca6f7e03e75103c58c366736a6a
m-taqii <busy.digital.master@gmail.com>
2026-04-11 19:58:10 +0500

    feat: add voice to speak

---
+++ voice/speak.py
@@ -0,0 +1,20 @@
+import asyncio
+import edge_tts
+import sys
+import subprocess
+import tempfile
+import os
+
+async def speak():
+    path = os.path.join(tempfile.gettempdir(), 'argus_out.mp3')
+    text = sys.argv[1] if len(sys.argv) > 1 else "Hey This is Argus. How are you doing today?"
+    voice = "en-US-AndrewMultilingualNeural"
+    rate = "-5%"
+    pitch = "-6Hz"
+
+    communicate = edge_tts.Communicate(text, voice, rate=rate, pitch=pitch)
+    await communicate.save(path)
+    subprocess.run(['ffplay', '-nodisp', '-autoexit', path])
+
+if __name__ == "__main__":
+    asyncio.run(speak())   
